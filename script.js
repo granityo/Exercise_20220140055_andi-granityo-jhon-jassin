87f828991e21d5a604cd743af1717c5c75f1e5cd
granityo <frhnspensa@gmail.com>
2024-03-28 05:20:11 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function showAlert() {
+    alert(Date(), tampil(), show());
+    Date();
+  
+    let tanggal = new date();
+    documentge.getElementByid("hasil").innerHTML = tanggal;
+  
+    function tampil() {
+      alert(
+        "NISSAN 34R TUNNING mobil drift yang masuk dalam salah satu kategori mobil terbaik"
+      );
+    }
+  
+    function show() {
+      alert("warna putih biru sudah melekat pada mobil ini ");
+    }
+  }
